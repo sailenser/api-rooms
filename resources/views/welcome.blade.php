7abcffb731c3f9a9c027d037ede20bf61c37727b
baranov.m <baranov.m@promail366.com>
2025-10-23 21:56:49 +0300

    вернул welcome

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Api backend</title>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <p>Api backend</p>
+    </body>
+</html>
